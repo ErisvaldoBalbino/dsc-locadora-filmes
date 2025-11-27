5ecd555c40fbb2ff6154c5ff21def01a08a61f78
Erisvaldo <erisvaldoapps@gmail.com>
2025-11-27 15:20:35 -0300

    feat: add security config

---
+++ src/main/java/ifrn/edu/br/locadora_filmes/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package ifrn.edu.br.locadora_filmes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**").permitAll()
+                .requestMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .headers(headers -> headers
+                .frameOptions(frameOptions -> frameOptions.disable())
+            );
+        
+        return http.build();
+    }
+}
+
