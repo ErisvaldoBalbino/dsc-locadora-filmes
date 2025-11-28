8c5e5ea5c1918c8ce52a067eb7e28837cb6a0e7d
Erisvaldo <erisvaldoapps@gmail.com>
2025-11-28 16:34:34 -0300

    chore: remove unused imports

--- src/main/java/ifrn/edu/br/locadora_filmes/dto/requests/GeneroUpdateDTO.java
+++ src/main/java/ifrn/edu/br/locadora_filmes/dto/requests/GeneroUpdateDTO.java
@@ -1,8 +1,6 @@
 package ifrn.edu.br.locadora_filmes.dto.requests;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
