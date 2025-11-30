88935070c8e07e6563d4ee46a7f24f5855ec3aa4
Erisvaldo <erisvaldoapps@gmail.com>
2025-11-30 15:40:55 -0300

    test: create filme

--- src/test/java/ifrn/edu/br/locadora_filmes/FilmeServiceTest.java
+++ src/test/java/ifrn/edu/br/locadora_filmes/FilmeServiceTest.java
@@ -11,6 +11,8 @@ import ifrn.edu.br.locadora_filmes.service.FilmeService;
 import ifrn.edu.br.locadora_filmes.service.GeneroService;
 import ifrn.edu.br.locadora_filmes.repository.FilmeRepository;
 import ifrn.edu.br.locadora_filmes.repository.GeneroRepository;
+import ifrn.edu.br.locadora_filmes.dto.requests.FilmeCreateDTO;
+import ifrn.edu.br.locadora_filmes.dto.responses.FilmeResponseDTO;
 import ifrn.edu.br.locadora_filmes.model.Filme;
 import ifrn.edu.br.locadora_filmes.model.Genero;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,10 +27,46 @@ public class FilmeServiceTest {
     private FilmeService filmeService;
 
     @Mock
+    private GeneroRepository generoRepository;
+
+    @Mock
     private FilmeRepository filmeRepository;
 
     @Test
-    void getFilmeById() {
+    void testCreateFilme() {
+        FilmeCreateDTO filmeDTO = new FilmeCreateDTO();
+
+        Genero genero = new Genero();
+        genero.setId(1L);
+        genero.setNome("Ação");
+
+        filmeDTO.setTitulo("Filme teste");
+        filmeDTO.setAno(2025);
+        filmeDTO.setQuantidade_total(10);
+        filmeDTO.setGeneroId(1L);
+
+        Filme filmeSalvo = new Filme();
+        filmeSalvo.setId(1L);
+        filmeSalvo.setTitulo("Filme teste");
+        filmeSalvo.setAno(2025);
+        filmeSalvo.setQuantidade_total(10);
+        filmeSalvo.setGenero(genero);
+
+        Mockito.when(filmeRepository.existsByTitulo("Filme teste")).thenReturn(false);
+        Mockito.when(generoRepository.findById(1L)).thenReturn(Optional.of(genero));
+        Mockito.when(filmeRepository.save(Mockito.any(Filme.class))).thenReturn(filmeSalvo);
+
+        FilmeResponseDTO resultado = filmeService.salvar(filmeDTO);
+
+        assertNotNull(resultado);
+        assertEquals("Filme teste", resultado.getTitulo());
+        assertEquals(2025, resultado.getAno());
+        assertEquals(10, resultado.getQuantidade_total());
+        assertEquals("Ação", resultado.getGenero());
+    }
+
+    @Test
+    void testGetFilmeById() {
         long filmeId = 1L;
 
         Genero mockGenero = new Genero();
